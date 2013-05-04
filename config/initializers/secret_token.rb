956837dd87d56f78ed12bc6c7281060b0c85aef8
Alexey Chekunkov <webdev@ashel.ru>
2013-05-05 02:11:03 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omnomnom::Application.config.secret_token = '55fd75590ee719bf2a0ea6d5fd7528683e05935cf44c60c1dd38effc0cea967d2ea11ddfa7a5af1bc2cc79ab46caffe7e1cc2926ed7570941a17d857510c8c75'
